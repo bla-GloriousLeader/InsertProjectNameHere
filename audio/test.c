5fc70a9d417484def13d1f7a31e664f710abbbdb
bla <bla@fuckyoudontspammyemailovereverything.com>
2013-06-25 17:57:22 -0500

    Compiled opus, put the headers and libs in their own foder to make them easier to toy with

--- audio/test.c
+++ audio/test.c
@@ -3,15 +3,6 @@
 #include "portaudio.h"
 #include <math.h>
 
-typedef struct
-{
-    float left_phase;
-    float right_phase;
-}paTestData;
-/* This routine will be called by the PortAudio engine when audio is needed.
-   It may called at interrupt level on some machines so don't do anything
-   that could mess up the system like calling malloc() or free().
-*/ 
 static int patestCallback( const void *inputBuffer, void *outputBuffer,
                            unsigned long framesPerBuffer,
                            const PaStreamCallbackTimeInfo* timeInfo,
@@ -19,29 +10,25 @@ static int patestCallback( const void *inputBuffer, void *outputBuffer,
                            void *userData )
 {
     /* Cast data passed through stream to our structure. */
-    paTestData *data = (paTestData*)userData; 
+    float *data = (float*)userData; 
     float *out = (float*)outputBuffer;
     unsigned int i;
-    (void) inputBuffer; /* Prevent unused variable warning. */
+    (void) outputBuffer; /* Prevent unused variable warning. */
     
     for( i=0; i<framesPerBuffer; i++ )
     {
-        *out++ = data->left_phase > 0 ? 1.0f : 0.0f;  /* left */
-        *out++ = data->right_phase > 0 ? 1.0f : 0.0f;  /* right */
+        *out++ = *data > 0 ? 1.0f : 0.0f;
         /* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
-        data->left_phase += 0.006f;
+		*data+= 0.06f;
         /* When signal reaches top, drop back down. */
-        if( data->left_phase >= 1.0f ) data->left_phase -= 2.0f;
-        /* higher pitch so we can distinguish left and right. */
-        data->right_phase += 0.01f;
-        if( data->right_phase >= 1.0f ) data->right_phase -= 2.0f;
+        if( *data >= 1.0f ) *data -= 2.0f;
     }
     return 0;
 }
 
 int main(void)
 {
-	paTestData udata;
+	float udata;
 	PaStream *stream;
 	PaError err;
 	
@@ -52,7 +39,7 @@ int main(void)
 		return 0;
 	}
 	
-	err = Pa_OpenDefaultStream(&stream, 0, 2, paFloat32, 44100, 256, patestCallback, &udata);
+	err = Pa_OpenDefaultStream(&stream, 1, 0, paFloat32, 44100, 256, patestCallback, &udata);
 	if(err != paNoError)
 	{
 		printf("SHIT!");
